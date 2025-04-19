1ee2b2184afc6226ca3e0e9d9cee366f365a23e6
Lucky <ptl.om.22@gmail.com>
2025-04-19 12:38:35 +0530

    Tuple Practice
    Complated Tuple Practice in Python

---
+++ Ai Qustions Practice/Tuple_Count_Index.py
@@ -0,0 +1,6 @@
+#Count & Index in Tuple
+#CODE BY LUCKY
+
+fruits = ("apple", "banana", "apple", "cherry", "banana")
+print(fruits.count("apple"))     # Count the number of times "apple" appears in the tuple
+print(fruits.index("cherry"))    # Find the index of the first occurrence of "cherry" in the tuple
---
+++ Ai Qustions Practice/Tuple_Info.py
@@ -0,0 +1,6 @@
+#Tuple Info
+#CODE BY LUCKY
+
+movies = ('Stranger_Things', 'Mr.Robot', 'Breaking_Bad')
+print(movies)
+print(movies[1])
